4bb35fa696c9e7cc5820d79e7e98d4da06ddf934
Paige <paigeberliner@gmail.com>
2024-06-22 13:57:55 -0400

    Complete Lab

--- server/app.py
+++ server/app.py
@@ -20,7 +20,54 @@ def index():
     body = {'message': 'Flask SQLAlchemy Lab 1'}
     return make_response(body, 200)
 
-# Add views here
+@app.route('/earthquakes/<int:id>')
+def earthquake_by_id(id):
+    earthquake = Earthquake.query.filter(Earthquake.id == id).first()
+    if earthquake:
+        response_body = {
+            'id': earthquake.id,
+            'magnitude': earthquake.magnitude,
+            'location': earthquake.location,
+            'year': earthquake.year
+        }
+        response_status = 200 
+    else: 
+        response_body = {
+            'message': 'Earthquake 9999 not found.'
+        }
+        response_status = 404
+
+    response = make_response(response_body, response_status)
+    return response
+   
+
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def earthquake_by_magnitude(magnitude):
+    earthquakes = Earthquake.query.filter(Earthquake.magnitude > magnitude).all()
+    if earthquakes:
+        response_body = {
+            "count": len(earthquakes),
+            "quakes": [
+                {
+                    'id': earthquake.id,
+                    'magnitude': earthquake.magnitude,
+                    'location': earthquake.location,
+                    'year': earthquake.year
+                }
+                for earthquake in earthquakes
+            ]
+        }
+        response_status = 200
+    else:
+        response_body = {
+            "count": 0,
+            "quakes": []
+        }
+        response_status = 200
+
+    
+    response = make_response(response_body, response_status)
+    return response
 
 
 if __name__ == '__main__':
